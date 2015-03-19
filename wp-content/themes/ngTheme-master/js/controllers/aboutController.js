adba5f080084636d23594827e36d82fcf442358c
jaggedrose <jennierowe@hotmail.com>
2015-03-19 14:31:38 +0100

    Om oss sida controller & view

---
+++ wp-content/themes/ngTheme-master/js/controllers/aboutController.js
@@ -0,0 +1,11 @@
+app.controller("aboutController", ["$scope", "Pages", "$sce", "$routeParams", function($scope, Pages, $sce, $routeParams) {
+	console.log("aboutController Working! routeParams: ", $routeParams);
+	// Get the page
+	Pages.get($routeParams.slug);
+
+	// Check for broadcast
+	$scope.$on("gotPageData", function(event, data) {
+		console.log("aboutController on gotPageData: ", data);
+		$scope.allPages = data;
+	});
+}]);
