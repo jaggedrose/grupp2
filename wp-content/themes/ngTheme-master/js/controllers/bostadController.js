4060655cc581327925244ab464641c7418793ad6
Molotovs <henrikn@live.se>
2015-03-19 14:27:21 +0100

    Created a bostadController /Henrik, Magnus, Santino

--- wp-content/themes/ngTheme-master/js/controllers/bostadController.js
+++ wp-content/themes/ngTheme-master/js/controllers/bostadController.js
@@ -1,1 +1,14 @@
-app.controller("")
+app.controller("bostadController", ["$scope", "Bostad" ,"$routeParams", "SITE_INFO", function($scope, Bostad, $routeParams, SITE_INFO) {
+	console.log("bostadController is alive! params: ", $routeParams);
+
+	Bostad.find($routeParams);
+	$scope.partialsDir = SITE_INFO.partials;
+
+	$scope.carouselInterval = 5000;
+
+	$scope.$on("foundBostad", function(event, data) {
+		console.log("bostadController on foundBostad: ", data);
+		if (data.length === 0) {return;}
+		$scope.bostad = data[0];
+	});
+}]);
