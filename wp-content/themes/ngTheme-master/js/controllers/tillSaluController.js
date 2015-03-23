0aeff8a81e9fee48b455b5720be05ec100c16941
SantinoPetrovic <santinopetrovic@gmail.com>
2015-03-23 14:35:44 +0100

    Comments becomes code!

--- wp-content/themes/ngTheme-master/js/controllers/tillSaluController.js
+++ wp-content/themes/ngTheme-master/js/controllers/tillSaluController.js
@@ -1,11 +1,11 @@
-// app.controller("tillSaluController", ["$scope", "Pages", "$sce", "$routeParams", function($scope, Pages, $sce, $routeParams) {
-// 	console.log("tillSaluController Working! routeParams: ", $routeParams);
-// 	// Get the page
-// 	Pages.get($routeParams.slug);
+app.controller("tillSaluController", ["$scope", "Pages", "$sce", "$routeParams", function($scope, Pages, $sce, $routeParams) {
+	console.log("tillSaluController Working! routeParams: ", $routeParams);
+	// Get the page
+	Pages.get($routeParams.slug);
 
-// 	// Check for broadcast
-// 	$scope.$on("gotPageData", function(event, data) {
-// 		console.log("tillSaluController on gotPageData: ", data);
-// 		$scope.page = data[0];
-// 	});
-// }]);
+	// Check for broadcast
+	$scope.$on("gotPageData", function(event, data) {
+		console.log("tillSaluController on gotPageData: ", data);
+		$scope.page = data[0];
+	});
+}]);
