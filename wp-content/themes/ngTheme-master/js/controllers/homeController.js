932092e24f4f39c40486636fd4b1d3bc53edb006
jaggedrose <jennierowe@hotmail.com>
2015-03-25 16:25:28 +0100

    Added carousel to "home", need to get media to show

--- wp-content/themes/ngTheme-master/js/controllers/homeController.js
+++ wp-content/themes/ngTheme-master/js/controllers/homeController.js
@@ -1,16 +1,18 @@
 //"ngTheme" home controller.
 //dependent on $scope && WPService being injected to run
-app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
-  console.log("homeController alive!");
-  //get all pages
-  Pages.get();
+app.controller("homeController", ["$scope", "Pages", "$sce", "$routeParams", function($scope, Pages, $sce, $routeParams) {
+	console.log("homeController alive! routeParams: ", $routeParams);
+	
+	//get page
+	Pages.get("hem");
 
-  // EXAMPLE LISTENER TO A $broadcast COMING FROM WPRest SERVICE!!!
-  //listening for the "gotPageData" broadcast on $http success
-  $scope.$on("gotPageData", function(event, data) {
-    console.log("homeController on gotPageData: ", data);
-    $scope.pages = data;
+	$scope.carouselInterval = 5000;
 
-  });
+	// EXAMPLE LISTENER TO A $broadcast COMING FROM WPRest SERVICE!!!
+	//listening for the "gotPageData" broadcast on $http success
+	$scope.$on("gotPageData", function(event, data) {
+		console.log("homeController on gotPageData: ", data);
+		$scope.page = data[0];
+	});
   
 }]);
