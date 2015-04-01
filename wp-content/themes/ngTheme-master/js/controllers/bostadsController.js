9955de8a973610c25407ee895ccd65d50b7d3740
jaggedrose <jennierowe@hotmail.com>
2015-04-01 15:25:44 +0200

    New Db dump, fixed map redirect problem (it was inside an a tag), changed img size so all are the same & commented out console.logs

--- wp-content/themes/ngTheme-master/js/controllers/bostadsController.js
+++ wp-content/themes/ngTheme-master/js/controllers/bostadsController.js
@@ -1,6 +1,6 @@
 
 app.controller("bostadsController", ["$scope", "Bostad" ,"$routeParams", "SITE_INFO", function($scope, Bostad, $routeParams, SITE_INFO) {
-	console.log("bostadsController is alive! params: ", $routeParams);
+	// console.log("bostadsController is alive! params: ", $routeParams);
 
 	Bostad.find($routeParams);
 	$scope.partialsDir = SITE_INFO.partials;
@@ -8,7 +8,7 @@ app.controller("bostadsController", ["$scope", "Bostad" ,"$routeParams", "SITE_I
 	$scope.carouselInterval = 5000;
 
 	$scope.$on("foundBostad", function(event, data) {
-		console.log("bostadsController on foundBostad: ", data);
+		// console.log("bostadsController on foundBostad: ", data);
 		if (data.length === 0) {return;}
 		$scope.bostad = data[0];
 	});
--- wp-content/themes/ngTheme-master/js/controllers/tillSaluController.js
+++ wp-content/themes/ngTheme-master/js/controllers/tillSaluController.js
@@ -1,11 +1,11 @@
 app.controller("tillSaluController", ["$scope", "Pages", "Bostad", "$sce", "$routeParams", function($scope, Pages, Bostad, $sce, $routeParams) {
-	console.log("tillSaluController Working! routeParams: ", $routeParams);
+	// console.log("tillSaluController Working! routeParams: ", $routeParams);
 	// Get the page
 	Pages.get("till-salu");
 
 	// Check for broadcast
 	$scope.$on("gotPageData", function(event, data) {
-		console.log("tillSaluController on gotPageData: ", data);
+		// console.log("tillSaluController on gotPageData: ", data);
 		$scope.page = data[0];
 	});
 
